f2cd6101046719b05ef2d1c486a4183303a2ce28
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-04-10 17:58:34 +0300

    Task

---
+++ Troelson/FunWithCSharpAsync/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunWithCSharpAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(" Fun With Async ===>");
+            // Для подсказки Visual Studio модернизировать проект до версии C# 7.1.
+            List<int> L = default;
+            string message = await DoWorkAsync();
+            Console.WriteLine(message);
+            Console.WriteLine(DoWork());
+            Console.WriteLine("Completed");
+
+            await MethodReturningVoidAsync();
+            Console.WriteLine("Void method complete");
+
+            await MultiAwaits();
+
+            Console.ReadLine();
+        }
+        static string DoWork()
+        {
+            Thread.Sleep(1_000);
+            return "Done with work1!";
+        }
+        static async Task<string> DoWorkAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1_000);
+                return "Done with work!";
+            });
+        }
+        static async Task MethodReturningVoidAsync()
+        {
+            await Task.Run(() =>
+            { /* Выполнить какую-то работу... */
+                Thread.Sleep(4_000);
+            });
+            Console.WriteLine("Void method completed");
+        }
+        static async Task MultiAwaits()
+        {
+            await Task.Run(() => { Thread.Sleep(2_000); });
+            Console.WriteLine("Done with first task!");
+            await Task.Run(() => { Thread.Sleep(2_000); });
+            Console.WriteLine("Done with second task!");
+            await Task.Run(() => { Thread.Sleep(2_000); });
+            Console.WriteLine("Done with third task!");
+        }
+        static async Task<string> MethodWithTryCatch()
+        {
+            try
+            {
+                // Выполнить некоторую работу.
+
+                return "Hello";
+            }
+            catch (Exception ex)
+            {
+                await LogTheErrors();
+                throw;
+            }
+            finally
+            {
+                await DoMagicCleanUp();
+            }
+        }
+
+        private static Task LogTheErrors()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task DoMagicCleanUp()
+        {
+            throw new NotImplementedException();
+        }
+        static async ValueTask<int> ReturnAnlnt()
+        {
+            await Task.Delay(1000);
+            return 5;
+        }
+        static async Task MethodWithProblems(int firstParam, int secondParam)
+        {
+            Console.WriteLine("Enter");
+            await Task.Run(() =>
+            {
+                // Вызвать длительно выполняющийся метод.
+                Thread.Sleep(2000);
+                Console.WriteLine("First Complete");
+                // Вызвать еще один длительно выполняющийся метод, который терпит
+                // неудачу из-за того, что значение второго параметра выходит
+                //за пределы допустимого диапазона.
+                Console.WriteLine("Something bad happened");
+            });
+        }
+        static async Task MethodWithProblemsFixed(int firstParam, int secondParam)
+        {
+            Console.WriteLine("Enter");
+            if (secondParam < 0)
+            {
+                Console.WriteLine("Bad data");
+                return;
+            }
+            await actualImplementation();
+            async Task actualImplementation()
+            {
+                await Task.Run(() =>
+                {
+                    // Вызвать длительно выполняющийся метод.
+                    Thread.Sleep(2_000);
+                    Console.WriteLine("First Complete");
+                    // Вызвать еще один длительно выполняющийся метод, который терпит
+                    // неудачу из-за того, что значение второго параметра выходит
+                    // за пределы допустимого диапазона.
+                    Console.WriteLine("Something bad happened");
+                });
+            }
+        }
+    }
+}
---
+++ Troelson/PLINQDataProcessingWithCancellation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQDataProcessingWithCancellation
+{
+    class Program
+    {
+        static CancellationTokenSource cancelToken = new CancellationTokenSource();
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Press any key to start processing");
+                // Нажмите любую клавишу для начала обработки
+                Console.ReadKey();
+                Console.WriteLine("Processing");
+                Task.Factory.StartNew(() => ProcessIntData());
+                Console.Write("Enter Q to quit: ");
+                // Введите Q для выхода:
+                string answer = Console.ReadLine();
+                // Желает ли пользователь выйти?
+                if (answer.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    cancelToken.Cancel();
+                    break;
+                }
+            } while (true);
+            Console.ReadLine();
+        }
+        static void ProcessIntData()
+        {
+            // Получить очень большой массив целых чисел.
+            int[] source = Enumerable.Range(1, 10000000).ToArray();
+            // Найти числа, для которых истинно условие num % 3 == 0,и возвратить их в убывающем порядке.
+            int[] modThreelsZero = null;
+            try
+            {
+                modThreelsZero =
+                (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                 where num % 3 == 0
+                 orderby num descending
+                 select num).ToArray();
+                Console.WriteLine();
+                // Вывести количество найденных чисел.
+                Console.WriteLine($"Found {modThreelsZero.Count()} numbers that match query!");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        
+    }
+}
